a3c98da543dda33521685fc9bac0ff529c8dca52
Pat Marinich <patrick.marinich@yahoo.com>
2025-01-29 23:16:15 -0500

    winter tourney page update

--- player_profile/PlayerProfile.py
+++ player_profile/PlayerProfile.py
@@ -1660,6 +1660,7 @@ def convertHTMLtoPDF(filename):
   merger.append("player_profile/pre_made_pdf/Kartnite Title Page.pdf")
   merger.append(output)
   merger.append("player_profile/pre_made_pdf/Kartnite Championship season 6.pdf")
+  merger.append("player_profile/pre_made_pdf/Okemo Vehicle Elimination Tournament.pdf")
 
   merger.write('Kartnite Stats - ' + today + '.pdf')
   merger.close()
