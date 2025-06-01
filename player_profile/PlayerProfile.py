6a16303381e8de4f99c756bc17c9056e54089fdb
Pat Marinich <patrick.marinich@yahoo.com>
2025-06-01 16:27:29 -0400

    add in events summary, and add date to championship

--- player_profile/PlayerProfile.py
+++ player_profile/PlayerProfile.py
@@ -1809,6 +1809,7 @@ def convertHTMLtoPDF(filename):
 
   merger = PdfWriter()
   merger.append("player_profile/pre_made_pdf/Kartnite Title Page.pdf")
+  merger.append("player_profile/pre_made_pdf/Kartnite Events Summary - Living Document.pdf")
   merger.append("player_profile/pre_made_pdf/Kartnite Championship season 6.pdf")
   merger.append("player_profile/pre_made_pdf/Okemo Vehicle Elimination Tournament.pdf")
   merger.append("player_profile/pre_made_pdf/Kartnite knockout tournament.pdf")
