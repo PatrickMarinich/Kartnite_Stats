551fd085c742560d3efac442c77486d01355d2fc
Pat <92064826+PatrickMarinich@users.noreply.github.com>
2025-06-30 23:30:03 -0400

    Update Constants.py

--- player_profile/Constants.py
+++ player_profile/Constants.py
@@ -166,7 +166,7 @@ MIKE_SEASON7_AWARDS = ['Season VII \"Must get the win, must get the win\" Award 
 SHANE_SCORE_AWARDS = ['Perfect 120 - Nov 24th 2023']
 KEVIN_SCORE_AWARDS = ['Perfect 120 - Feb 10th 2025','Scored 115: x2', 'Scored 114: x3' ,'Scored 112: x2 ','Scored 111: x2', 'Scored 110: x2']
 DEMITRI_SCORE_AWARDS = ['Perfect 120 - Sept 3rd 2024','Scored 117 x1', 'Scored 115 x1','Scored 112: x1','Scored 111: x1', 'Scored 110: x2']
-PAT_SCORE_AWARDS = ['Scored 114: x3','Scored 111: x5','Scored 110: x2']
+PAT_SCORE_AWARDS = ['Scored 114: x4','Scored 111: x5','Scored 110: x2']
 CHIRS_SCORE_AWARDS = ['Scored 111: x1','Scored 110: x2']
 #-----------------
 
